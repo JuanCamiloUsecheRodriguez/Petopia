7f3a42aed1d22af04e39448025e1866c26b5fca8
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-10-26 23:08:05 -0500

    Form addPet con state

--- imports/ui/AddPetForm.js
+++ imports/ui/AddPetForm.js
@@ -3,9 +3,42 @@ import {Container, Row, Col, Button} from 'reactstrap';
 
 export default class AddPetForm extends Component {
 
+  constructor(props){
+    super(props);
 
-  
+    this.state={
+        name: '',
+        ageYears:0,
+        ageMonths: 0,
+        petsonality: '',
+        gender: '',
+        breed:'',
+        sterilized: true,
+        likes: '',
+        dislikes: '',
+        story: '',
+        image: ''
+    };
+
+    //bind
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+    // EventHandlers
+
+    handleChange(event) {
+      const value = event.target.value;
+      this.setState({
+        [event.target.name]: value
+      });
+    }
+
+
+
   render() {
+
+    const {name, ageYears, ageMonths, petsonality, gender, breed, sterilized, likes, dislikes, story, image} = this.state
     return (
       <div>
           <Container>
@@ -19,24 +52,31 @@ export default class AddPetForm extends Component {
                     <form className='form-style-8'>
                     
                       <img src="https://images.pexels.com/photos/1302290/pexels-photo-1302290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" className="img-fluid" alt="petimage"/>
-                      <div class="form-group">
-                        <label for="SubmitPhoto">Choose a nice picture!</label>
-                        <input type="file" class="form-control-file" id="petPhoto"/>
+                      <div className="form-group">
+                        <label htmlFor="SubmitPhoto">Choose a nice picture!</label>
+                        <input type="file" className="form-control-file" id="petPhoto" name='image' value={image} onChange={this.handleChange}/>
                       </div>
                     
-                    
-
-                    
-                      Hi!, my name is <input type='text' placeholder='Lupe'/> and I am <input type='number' placeholder='7' min='0'/> years and <input type='number' placeholder='5' min='0'/>months old.<br/>
-                      My gender is <select><option>Female</option><option>Male</option></select> and my breed is <input type='text' placeholder='unique'/>. 
-                      I have a <input type='text' placeholder='calm'/> petsonality. I really love <input type='text' placeholder='sleeping'/>, I dislike <input type='text' placeholder='loud noises'/>.
+                      Hi!, my name is 
+                      <input type='text' placeholder='Lupe' name='name' value={name} onChange={this.handleChange}/> 
+                      and I am 
+                      <input type='number' placeholder='7' min='0' name='ageYears' value={ageYears} onChange={this.handleChange}/> 
+                      years and 
+                      <input type='number' placeholder='5' min='0' name='ageMonths' value={ageMonths} onChange={this.handleChange}/>months old.<br/>
+                      My gender is 
+                      <select name='gender'><option>Female</option><option>Male</option></select> and my breed is 
+                      <input type='text' placeholder='unique' name='breed' value={breed} onChange={this.handleChange}/>. 
+                      I have a 
+                      <input type='text' placeholder='calm' name='petsonality' value={petsonality} onChange={this.handleChange}/> petsonality. I really love 
+                      <input type='text' placeholder='sleeping' name='likes' value={likes} onChange={this.handleChange}/>, I dislike 
+                      <input type='text' placeholder='loud noises' name='dislikes' value={dislikes} onChange={this.handleChange}/>.
                       <br/>
-                      <div class="form-group purple-border">
-                        <label for="Story">Here is my story</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea4" rows="3"></textarea>
+                      <div className="form-group purple-border">
+                        <label htmlFor="Story">Here is my story</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea4" rows="3" name='story' value={story} onChange={this.handleChange}></textarea>
                       </div>
 
-                      Sterilized? <select><option>Yes</option><option>No</option></select>
+                      Sterilized? <select name='sterilized'><option>Yes</option><option>No</option></select>
                       <br/>
 
                       <Button>Submit</Button>
