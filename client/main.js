223b3fae6303843fab72a65c5d92f4c8d04e12f8
Juan Camilo Usche <jc.useche10@uniandes.edu.co>
2018-10-26 20:22:25 -0500

    navbar y routing

--- client/main.js
+++ client/main.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LandingPage from '../imports/ui/LandingPage.js';
+import App from "../imports/ui/App.js";
 import './main.html';
 
 Meteor.startup(() => {
-  render(<LandingPage/>,
-    document.getElementById('target'));
+  render(
+    <Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={LandingPage}/>
+      </Switch>
+    </App>
+  </Router>
+  ,document.getElementById('target'));
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Navbar from './Navbar.js';
 
-export default class App extends Component {
-
-    constructor(props){
-        super(props);
-        this.state={
-            
-        }
-    }
-
+export default class App extends React.Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+   {
+      return (
+        <div>
+         <Navbar/>
+
+          <div className="content">
+            {this.props.children}
+          </div>
+          <footer className="bg-dark">
+            <div className="footer_makers" align="center">Por Juan Camilo Useche y Zulma Castañeda</div>
+            <img className="MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" alt= "MIT license logo"/>
+          </footer>
+        </div>
+      );
+    } 
   }
 }
