b6ac3482c2b6626a3236300d5dfc1ca09614df01
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-12-04 11:12:38 -0500

    paginacion correcta, dañandole cosas a camilo :v

--- imports/ui/viewPets.js
+++ imports/ui/viewPets.js
@@ -18,7 +18,11 @@ class ViewPets extends Component {
             cats : false,
             currentPage: 1,
             petPerPage: 9,
-            currentPets:[]
+            currentPets:[],
+            arrCats: [],
+            arrDogs:[]
+
+
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
@@ -31,6 +35,7 @@ class ViewPets extends Component {
     }
 
     handleChange(event) {
+        this.sortPets();
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
@@ -45,8 +50,8 @@ class ViewPets extends Component {
         let pets =[]
         
         currentpets.map((r,i)=>{
-            if(r.rCity.includes(this.props.user.profile.city)){
-            if (r.species == 'Cat' && this.state.cats){
+            
+           
                 let desc = 'Im a very '+r.petsonality+' '+r.species+' who loves '+r.likes+'. I dislike '+r.dislikes+', I am '+r.ageYears+' years and '+r.ageMonths+' months old.' ;
                 pets.push(
                     <div class="col-sm">
@@ -61,61 +66,135 @@ class ViewPets extends Component {
                     </div>
                 ); 
                 
-            }
             
-            if(r.species == 'Dog' && this.state.dogs){
-                let desc = 'Im a very '+r.petsonality+' '+r.species+' who loves '+r.likes+'. I dislike '+r.dislikes+', I am '+r.ageYears+' years and '+r.ageMonths+' months old.' ;
-
-                pets.push(
-                    <div class="col-sm">
-                    <PetCard
-                    key={i}
-                    image='https://images.pexels.com/photos/159541/wildlife-photography-pet-photography-dog-animal-159541.jpeg?cs=srgb&dl=adorable-animal-breed-159541.jpg&fm=jpg'
-                    name={ r.name}
-                    description =  {desc}
-                    idPet={r._id}
-                    responsibleId={r.responsibleId}
-                    />
-                    </div>
+            
+            
                     
-                );
-                
-                        }
-                    }
                 });
         return pets;
     };
+
+    sortPets(){
+
+        const cats = [];
+        const dogs =[];
+
+
+        this.props.pets.map((r,i)=>{
+            if(r.species == 'Cat'){
+                cats.push(r);
+            }else{
+                dogs.push(r);
+            }
+            
+        });
+
+        
+
+        this.setState({
+            arrCats: cats,
+            arrDogs: dogs
+        });
+
+    }
+
+    componentWillReceiveProps(){
+        this.sortPets()
+    }
+
+    paginate(petsArray){
+        const currentPage = this.state.currentPage;
+        let currentminus = currentPage-15;
+        let currentplus = currentPage+15;
+        const petsPerPage = this.state.petPerPage;
+
+        const indexLast = currentPage * petsPerPage;
+        const indexFirst = indexLast-petsPerPage;
+        const current = petsArray.slice(indexFirst,indexLast);
+        let pageNumbers = [];
+
+        for (let i = 1; i <= Math.ceil(petsArray.length / petsPerPage); i++) {
+            pageNumbers.push(i);
+           
+            
+        }
+        let pages =[];
+
+        if(currentminus<0){
+            currentplus = currentplus-currentminus;
+            currentminus =0;
+            
+        }else if(currentplus>indexLast-1){
+            currentplus=indexLast-1;
+        }
+      
+       
+        
+        
+        
+        pageNumbers = pageNumbers.slice(currentminus,currentplus);
+
+        pageNumbers.map(number => {
+            pages.push (
+              <li
+                key={number}
+                id={number}
+                onClick={this.handleClick}
+              >
+                {number}
+              </li>
+            );
+          });
+   
+          
+
+          return pages;
+    }
+
+    arrayselect(){
+        if (this.state.dogs && this.state.cats){
+            console.log('d');
+            return this.paginate(this.props.pets);
+        }else if(this.state.dogs){
+            return this.paginate(this.state.arrDogs);
+        }else if(this.state.cats){
+            return this.paginate(this.state.arrCats);
+        }
+
+    }
+
+    renderselect(){
+        const currentPage = this.state.currentPage;
+        const petsPerPage = this.state.petPerPage;
+
+        const indexLast = currentPage * petsPerPage;
+        const indexFirst = indexLast-petsPerPage;
+        let current = [];
+        if (this.state.dogs && this.state.cats){
+            console.log('d');
+            current = this.props.pets.slice(indexFirst,indexLast);
+            
+        }else if(this.state.dogs){
+            current = this.state.arrDogs.slice(indexFirst,indexLast);
+
+           
+        }else if(this.state.cats){
+            current = this.state.arrCats.slice(indexFirst,indexLast);
+            
+        }
+        return this.renderPets(current);
+
+    }
 
 
 
 
 
   render() {
-    const pets = this.props.pets;
-    const currentPage = this.state.currentPage;
-    const petsPerPage = this.state.petPerPage;
+    
 
-    const indexLast = currentPage * petsPerPage;
-    const indexFirst = indexLast-petsPerPage;
-    const current = this.props.pets.slice(indexFirst,indexLast);
 
-    const pageNumbers = [];
-
-    for (let i = 1; i <= Math.ceil(this.props.pets.length / petsPerPage); i++) {
-        pageNumbers.push(i);
-    }
-
-    const renderPageNumbers = pageNumbers.map(number => {
-        return (
-          <li
-            key={number}
-            id={number}
-            onClick={this.handleClick}
-          >
-            {number}
-          </li>
-        );
-      });
+    
 
 
     return (
@@ -134,15 +213,14 @@ class ViewPets extends Component {
             </div>
             
         <div class="row">
-        
+        {this.renderselect()}
 
-         {this.renderPets(current)}
-
-            
         </div>
-        <div class="row">
+        <div class="row text-center">
         <ul id="page-numbers">
-              {renderPageNumbers}
+              {
+                  this.arrayselect()
+              }
          </ul> 
           </div>
     </div>
