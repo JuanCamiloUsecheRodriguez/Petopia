6fde5499aa62312e6bc9451503eebe593e34641f
Laura Valeria Vanegas García <lv.vanegas10@uniandes.edu.co>
2018-12-05 18:43:25 -0500

    Intentación
    Mejorar indentación para facilitar lectura del código

--- imports/ui/PetCard.js
+++ imports/ui/PetCard.js
@@ -26,20 +26,16 @@ export default class PetCard extends Component {
           idPet,
         } = this.props;
     
-        return (
-          
+        return (          
           <div className='product-card text-center'>
-          <img src={image} alt={name}/>
-          <div className='card-margin'>
-            <div className='product-card-name'>
-          {name}
-        </div>
-            <p className='text-center'>
-          {description}
-        </p>
-            
-              <button className=' btn buy-button' onClick={this.adoptButton.bind(this,idPet)}>Adopt now</button>
-            </div>
+              <img src={image} alt={name}/>
+              <div className='card-margin'>
+                <div className='product-card-name'>
+                    {name}
+                </div>
+                <p className='text-center'>{description}</p>            
+                <button className=' btn buy-button' onClick={this.adoptButton.bind(this,idPet)}>Adopt now</button>
+              </div>
           </div>
         );
       }
